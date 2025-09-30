a2bb1ba5c4c30427ba24b0ff4dcbe478f947c962
rohan srivastava <rohan29srivastava@gmail.com>
2025-09-30 20:52:33 +0530

    leetcode

---
+++ traingular_sum_of_array.py
@@ -0,0 +1,10 @@
+from typing import List
+class Solution:
+    def triangularSum(self, nums: List[int]) -> int:
+        n=len(nums)-1
+        ans, A=nums[0], 1
+        for k in range(1, n+1):
+            A=A*(n-k+1)//k
+            ans=(ans+nums[k]*A)%10
+        return ans
+        
