339419eebe8cc0fcc0f225b5cc3edf662b20d81a
rohan srivastava <rohan29srivastava@gmail.com>
2025-10-28 20:15:23 +0530

    leetcode

---
+++ 043.Make_array_element_equal_to_zero.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def countValidSelections(self, nums: List[int]) -> int:
+        prefix, cnt=0, 0
+        Sum=sum(nums)
+        for x in nums:
+            prefix+=x
+            if x==0:
+                cnt+=2*(2*prefix==Sum)
+                cnt+=(abs(2*prefix-Sum)==1)
+        return cnt
