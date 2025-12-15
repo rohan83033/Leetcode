ead0d8ea19ffdade3a5c9065a425044e99020b0b
rohan srivastava <rohan29srivastava@gmail.com>
2025-12-15 18:04:49 +0530

    leetcode

---
+++ 092.number_of_smooth_descent_periods_of_stock.py
@@ -0,0 +1,9 @@
+from typing import List
+class Solution:
+    def getDescentPeriods(self, prices: List[int]) -> int:
+        sum, des, prev=0, 0, -1
+        for x in prices:
+            des=(-((x+1)==prev) & des)+1
+            sum+=des
+            prev=x
+        return sum
